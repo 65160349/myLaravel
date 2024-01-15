0f7fb7a3a764c767585ab03377159cdf3016b8c6
65160349 <65160349@go.buu.ac.th>
2024-01-15 19:55:04 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
+use App\Http\Controllers\C_titles;
 use Illuminate\Http;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\myController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::resource('titles', C_titles::class);
+
+Route::get('/home', [myController::class , 'index']);
 
 Route::get('/', function () {
     // return view('welcome');
