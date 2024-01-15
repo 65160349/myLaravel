3c054156d000ef46399df905982c16521b6da26b
65160349 <65160349@go.buu.ac.th>
2024-01-15 13:02:12 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
+use Illuminate\Http;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,26 +9,35 @@ use Illuminate\Http\Request;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
-    return view('welcome'); //welcome.blade.php
+    // return view('welcome');
+    return 'hi';
 });
 
-
-Route::get('/my-route', function(){
-    //return view('myroute');
-    //         Key    =>
+Route::get('/my-route', function () {
+    //echo "<h1> My Route Page</h1>";
+    //return view('')
+    //return viem('myroute');
     $data = ['val_a' => 'Hello World!'];
-    $data['val_b'] = "Laravel";
+    $data['val_b'] = "Lalaval";
     return view('myfolder.mypage',$data);
 });
 
+Route::post('/my-route',function(Request $req){
+    $data['myinput'] = $req -> input('myinput');
+    return view ('myroute',$data);
+});
 
-Route::post('/my-route', function(Request $req){
-    $data['myinput'] = $req->input('myinput');
-    return view('myroute', $data);
+Route::get('/my-display', function () {
+    return view('myfolder.mypage2');
+});
+
+Route::post('/my-display',function(Request $req) {
+    $data['myinputnumber'] = $req -> input('myinputnumber');
+    return view('myroute2', $data);
 });
